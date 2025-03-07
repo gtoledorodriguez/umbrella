992e939bf1399665315a9a42087b07ff65f12bf5
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-03-07 07:54:24 +0000

    added url and getting location

--- env_test.rb
+++ env_test.rb
@@ -1,4 +1,5 @@
 # /env_test.rb
 require "dotenv/load"
 
-pp ENV.fetch("PIRATE_WEATHER_API_KEY")
+pp ENV.fetch("PIRATE_WEATHER_KEY")
+pp ENV.fetch("GMAPS_KEY")
--- umbrella.rb
+++ umbrella.rb
@@ -1,3 +1,17 @@
 require "http"
 require "json"
 require "dotenv/load"
+
+puts "========================================\n    Will you need an umbrella today?    \n========================================"
+puts "Where are you?"
+loc = gets.chomp
+puts loc
+
+map_key = ENV.fetch("GMAPS_KEY")
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+map_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+loc+"&key="+map_key
+raw_gmaps_data = HTTP.get(map_url)
+parsed_gmaps_data = JSON.parse(raw_gmaps_data)
+print parsed_gmaps_data
+# pirate_weather_url = "https://api.pirateweather.net/forecast/"+pirate_weather_key+"/"+latitude+","+longitude
