361cb77987a1ec916520316c5df2aa56b5998082
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-03-07 08:11:16 +0000

    finished gmaps

--- umbrella.rb
+++ umbrella.rb
@@ -4,14 +4,22 @@ require "dotenv/load"
 
 puts "========================================\n    Will you need an umbrella today?    \n========================================"
 puts "Where are you?"
-loc = gets.chomp
-puts loc
+user_location = gets.chomp.gsub(" ", "%20")
+#user_location = "Chicago"
+puts "Checking the weather at #{user_location.gsub("%20", " ")}...."
 
 map_key = ENV.fetch("GMAPS_KEY")
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
-map_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+loc+"&key="+map_key
+map_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{map_key}"
 raw_gmaps_data = HTTP.get(map_url)
 parsed_gmaps_data = JSON.parse(raw_gmaps_data)
-print parsed_gmaps_data
+results = parsed_gmaps_data.fetch("results")
+first_result = results.at(0)
+geo = first_result.fetch("geometry")
+location = geo.fetch("location")
+latitude = location.fetch("lat")
+longitude = location.fetch("lng")
+pp latitude
+pp longitude
 # pirate_weather_url = "https://api.pirateweather.net/forecast/"+pirate_weather_key+"/"+latitude+","+longitude
